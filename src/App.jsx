2d5767a9c9a13e1988c1499a92b8d5763fba6bc4
MUKESH PAL <mukesh.mmp1234@gmail.com>
2024-04-23 01:41:13 +0530

    mlsa

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mukesh Pal",
+  title: "JAVA Deploper",
+  email: "MUKESH.PAL@studentambassadors.com",
+  gitHub: "mukprabhakar",
+  instagram: "mukprabhakar",
+  linkedIn: "mukprabhakar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mukprabhakar",
+  youTube: "Code-",
 };
 
 const primaryColor = "#4E567E";
